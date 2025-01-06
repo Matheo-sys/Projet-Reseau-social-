5f691a5e4b769e1f71b8d9a6f08921b940a9a547
Matheo-sys <matheosoriano.pro@gmail.com>
2025-01-06 10:50:15 +0100

    Ajout assert et test fonctions files et piles

---
+++ tests.py
@@ -0,0 +1,51 @@
+from fonctions import (
+    nouvelle_file,
+    enfiler,
+    defiler,
+    file_vide,
+    nouvelle_pile,
+    empiler,
+    depiler,
+    pile_vide,
+)
+
+def test_file():
+    
+    file = nouvelle_file()
+    assert file_vide(file) == True  
+
+    # Test de l'enfilement
+    enfiler(file, 1)
+    enfiler(file, 2)
+    enfiler(file, 3)
+    assert file_vide(file) == False  
+    assert file == [1, 2, 3] 
+
+    # Test du défilement
+    assert defiler(file) == 1 
+    assert defiler(file) == 2  
+    assert file == [3]  
+    assert defiler(file) == 3  
+    assert file_vide(file) == True  
+
+
+def test_pile():
+
+    pile = nouvelle_pile()
+    assert pile_vide(pile) == True 
+
+    # Test de l'empilement
+    empiler(pile, 1)
+    empiler(pile, 2)
+    empiler(pile, 3)
+    assert pile_vide(pile) == False  
+    assert pile == [1, 2, 3]  
+
+    # Test du dépilement
+    assert depiler(pile) == 3  
+    assert depiler(pile) == 2  
+    assert pile == [1] 
+    assert depiler(pile) == 1  
+    assert pile_vide(pile) == True  
+
+ 
