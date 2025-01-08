8e107f901bc180bd21e29ec534341c8d7031a919
Matheo-sys <matheosoriano.pro@gmail.com>
2025-01-08 14:12:12 +0100

    Definition generer graph et liste de prénom)

--- main.py
+++ main.py
@@ -1,4 +1,53 @@
 from outils import * 
+import random
+
+prenoms = [
+    # Prénoms masculins
+    "Adam", "Adrien", "Alexandre", "Antoine", "Arthur", "Benjamin", "Benoît", "Charles", 
+    "Christophe", "Daniel", "David", "Dylan", "Édouard", "Éric", "Fabien", "François", 
+    "Gabriel", "Geoffrey", "Guillaume", "Hugo", "Jacques", "Jean", "Jérémy", "Jonathan", 
+    "Julien", "Kevin", "Léo", "Louis", "Lucas", "Marc", "Mathieu", "Maxime", "Nicolas", 
+    "Noah", "Olivier", "Paul", "Pierre", "Quentin", "Raphaël", "Romain", "Samuel", 
+    "Simon", "Théo", "Thomas", "Victor", "Vincent", "William", "Xavier", "Yann", "Zacharie",
+
+    # Prénoms féminins
+    "Adèle", "Agnès", "Alice", "Amandine", "Amélie", "Anaïs", "Andréa", "Angélique", 
+    "Anne", "Aurélie", "Camille", "Caroline", "Catherine", "Charlotte", "Chloé", 
+    "Claire", "Clara", "Coralie", "Éléonore", "Élisabeth", "Élodie", "Émilie", 
+    "Emma", "Fanny", "Florence", "Gabrielle", "Hélène", "Inès", "Isabelle", 
+    "Jade", "Jeanne", "Julie", "Justine", "Laetitia", "Léa", "Louise", "Lucie", 
+    "Manon", "Margaux", "Marie", "Marina", "Mathilde", "Mélanie", "Nathalie", 
+    "Noémie", "Océane", "Pauline", "Sarah", "Sophie", "Valentine", "Victoria", "Yasmine", "Zoé",
+
+    # Prénoms neutres ou internationaux
+    "Alex", "Charlie", "Elliot", "Jules", "Milan", "Robin", "Sacha", "Sam", 
+    "Toni", "Chris", "Taylor", "Jordan", "Morgan", "Riley", "Jamie", "Cameron"
+]
+
+def generer_graphe(
+    oriente=False, 
+    nb_sommets=10, 
+    degre_min=1, 
+    degre_max=3, 
+    nb_communautes=1, 
+    distance_max=None, 
+    prenoms=None, 
+    fichier_sortie="graphe.txt"
+):
+  """
+    Génère un graphe aléatoire en respectant les contraintes données.
+    Enregistre la représentation textuelle dans un fichier.
+    
+    Arguments :
+        oriente (bool): Si le graphe est orienté ou non.
+        nb_sommets (int): Nombre de sommets dans le graphe.
+        degre_min (int): Degré minimum des sommets.
+        degre_max (int): Degré maximum des sommets.
+        nb_communautes (int): Nombre de communautés dans le graphe.
+        distance_max (int): Distance maximale entre deux sommets (pas implémenté dans cette version).
+        prenoms (list): Liste de prénoms à utiliser pour les sommets.
+        fichier_sortie (str): Nom du fichier de sortie.
+    """
 
 def temps_propagation(liste_adjacence, source, cible):
     """
